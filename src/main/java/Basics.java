0019627fde1dc801795343bccd8bdb823c421e94
sarahhe25 <sarahhe125@gmail.com>
2023-05-18 17:33:10 -0400

    update Basics.java

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -177,7 +177,7 @@ public class Basics {
          *                (e.g. arr[i] gives you the item at index i).
          */
 
-        for (int i = 0; i < arr.length; i += 2) {
+        for (int i = 1; i < arr.length; i += 2) {
             current_sum += arr[i];
         }
 
